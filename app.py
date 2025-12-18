99c46d387cc59bf49ce04470afbf99068a1a083c
Nishkal1249 <nishkalvandaana01@gmail.com>
2025-12-18 21:20:25 +0530

    Basic Form Validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,flash
 app=Flask(__name__)
 @app.route("/")
 def home():
@@ -13,12 +13,23 @@ def showskills():
     skills_list=["CSS","HTML","PYTHON"]
     return render_template("skills.html",skills=skills_list)
 
+app.secret_key="secret"
 @app.route('/contact',methods=["GET","POST"])
 def contact():
     if request.method=="POST":
         name=request.form.get("name")
         email=request.form.get("email")
-        return f"You are registred with email:{email}"
+        if not name:
+            flash("Name is required")
+            return redirect(url_for("contact"))
+        if not email:
+            flash("Email is required")
+            return redirect(url_for("contact"))
+        flash("Form submitted successfully")
+        return redirect(url_for('success'))
     return render_template("contact.html")
+@app.route('/success')
+def success():
+    return render_template("success.html")
 if __name__ == "__main__":
     app.run(debug=True)
